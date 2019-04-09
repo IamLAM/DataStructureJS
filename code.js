a93787128fa3b866258c5c04f276981df6511e3f
IamLAM <ingeniero@luismonge.com.mx>
2019-04-08 21:09:22 -0700

    insert values

--- code.js
+++ code.js
@@ -3,5 +3,6 @@
 let wordsArray = ["a", "b", "c", "d"];
 wordsArray[2] = "hi";
 console.log(wordsArray);
+//insert values
 wordsArray.unshift('I', 2, 'three'); //begin
 wordsArray.push(7, 'VIII', 9); //end
